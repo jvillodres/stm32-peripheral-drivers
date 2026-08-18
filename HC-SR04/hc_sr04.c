cfcd5160f51b357bdd2c3c13b0ab8357fc27b217
Juan Alejandro Villodres Perdomo <juan.villodres@utp.edu.co>
2026-08-17 23:07:45 -0500

    dev/hcsr04-L4-series-completed
    The driver for the ultrasonic sensor HCSR04 is complete and tested on the STM32 L4 series. TODO: Make documentation and implement a version for F1 series.

--- HC-SR04/hc_sr04.c
+++ HC-SR04/hc_sr04.c
@@ -1,1 +1,63 @@
 #include "hc_sr04.h"
+
+void HCSR04_Init(HCSR04_HandleTypeDef* hsensor) {
+	// GPIO Timers
+	if (hsensor->Echo_Port == GPIOA || hsensor->Trigger_Port == GPIOA) __HAL_RCC_GPIOA_CLK_ENABLE();
+	if (hsensor->Echo_Port == GPIOB || hsensor->Trigger_Port == GPIOB) __HAL_RCC_GPIOB_CLK_ENABLE();
+	if (hsensor->Echo_Port == GPIOC || hsensor->Trigger_Port == GPIOC) __HAL_RCC_GPIOC_CLK_ENABLE();
+
+	// GPIO Configurations
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	// Trigger Configuration
+	GPIO_InitStruct.Pin = hsensor->Trigger_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(hsensor->Trigger_Port, &GPIO_InitStruct);
+	HAL_GPIO_WritePin(hsensor->Trigger_Port, hsensor->Trigger_Pin, GPIO_PIN_RESET);
+
+	// Echo Configuration
+	GPIO_InitStruct.Pin = hsensor->Echo_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(hsensor->Echo_Port, &GPIO_InitStruct);
+
+	hsensor->Distance = 0.0f;
+}
+
+void HCSR04_Trigger(HCSR04_HandleTypeDef* hsensor) {
+	HAL_GPIO_WritePin(hsensor->Trigger_Port, hsensor->Trigger_Pin, GPIO_PIN_SET);
+
+	// Hold Trigger pin on for 10us
+	uint32_t start_time = HCSR04_Get_Us_Timestamp();
+	while((HCSR04_Get_Us_Timestamp() - start_time) < 10) {}
+
+	HAL_GPIO_WritePin(hsensor->Trigger_Port, hsensor->Trigger_Pin, GPIO_PIN_RESET);
+}
+
+void HCSR04_EXTI_Callback(HCSR04_HandleTypeDef* hsensor, uint16_t GPIO_Pin) {
+	if (GPIO_Pin == hsensor->Echo_Pin) {
+		if (HAL_GPIO_ReadPin(hsensor->Echo_Port, hsensor->Echo_Pin) == GPIO_PIN_SET) {
+			hsensor->T1 = HCSR04_Get_Us_Timestamp(); // Time when first rising edge
+			hsensor->First_Edge = 1;
+
+		} else {
+			if (hsensor->First_Edge == 1) {
+				hsensor->T2 = HCSR04_Get_Us_Timestamp(); // Time when falling edge
+
+				if (hsensor->T2 >= hsensor->T1) {
+					hsensor->Distance = (hsensor->T2 - hsensor->T1)/58; // Normal distance estimation
+				} else {
+					hsensor->Distance = (0xFFFFFFFF - hsensor->T1 + hsensor->T2)/58; // Overflowed distance estimation
+				}
+
+				hsensor->First_Edge = 0;
+			}
+		}
+	}
+}
+
+__weak uint32_t HCSR04_Get_Us_Timestamp(void) {
+	return 0;
+}
--- HC-SR04/hc_sr04.h
+++ HC-SR04/hc_sr04.h
@@ -5,25 +5,25 @@
 extern "C" {
 #endif
 
-#ifdef stm32f1xx
-#include "stm32f1xx_hal.h"
-#endif
-
 #include "stm32l4xx_hal.h"
 
 typedef struct {
-	GPIO_TypeDef*		Trigger_Port;	// GPIOX
-	uint16_t			Trigger_Pin;	// GPIO_PIN_X
+	GPIO_TypeDef*				Trigger_Port;	// GPIOX
+	uint16_t					Trigger_Pin;	// GPIO_PIN_X
+	GPIO_TypeDef*				Echo_Port;		// GPIOX
+	uint16_t					Echo_Pin;		// GPIO_PIN_X
 
-	GPIO_TypeDef*		Echo_Port;		// GPIOX
-	uint16_t			Echo_Pin;		// GPIO_PIN_X
-	TIM_TypeDef			Echo_Timer;		// &htimx
+	uint32_t					T1, T2;			// Flank interruption moments
+	uint8_t						First_Edge;		// Flag for first rising edge
+	uint16_t					Distance;		// x cm
 } HCSR04_HandleTypeDef;
 
 
-void HCSR04_Init(HCSR04_HandleTypeDef *hsensor); // Initialize sensor
-void HCSR04_Trigger(HCSR04_HandleTypeDef *hsensor); // Trigger sensor by software for measuring
-__weak void HCSR04_EXIT_Callback(HCSR04_HandleTypeDef *hsensor, uint16_t GPIO_Pin); // Callback for echo pin interruptions
+void HCSR04_Init(HCSR04_HandleTypeDef* hsensor); // Initialize sensor
+void HCSR04_Trigger(HCSR04_HandleTypeDef* hsensor); // Trigger sensor by software for measuring
+void HCSR04_EXTI_Callback(HCSR04_HandleTypeDef* hsensor, uint16_t GPIO_Pin); // Callback for echo pin interruptions
+
+uint32_t HCSR04_Get_Us_Timestamp(void);
 
 #ifdef __cplusplus
 }
